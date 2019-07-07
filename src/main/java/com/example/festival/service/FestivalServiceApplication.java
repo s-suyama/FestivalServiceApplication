e0919a6d280cbc68e6b105d45b448f73faa2df3f
shogo suyama <suman.yarn@gmail.com>
2019-07-07 15:31:50 +0900

    initial commit.

---
+++ src/main/java/com/example/festival/service/FestivalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.festival.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FestivalServiceApplication.class, args);
+  }
+
+}
